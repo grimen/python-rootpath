4ba25a5c23f19bc7da999e203e8e360452235a53
Jonas Grimfelt <grimen@gmail.com>
2019-03-10 05:59:47 -0400

    ci/bugfix: handle edge case with `__file__` in  a `__main__.py` being either file or directory - causing incorrectly configured sys load paths

--- rootpath/tests/__main__.py
+++ rootpath/tests/__main__.py
@@ -4,26 +4,27 @@
 # --------------------------------------
 
 import sys
+import os
 
 from os import path
 
 CURRENT_PATH = path.abspath(path.dirname(__file__))
-ROOT_PATH = path.abspath(path.join(CURRENT_PATH, '..', '..'))
+
+if path.isdir(__file__):
+    ROOT_PATH = path.abspath(path.join(CURRENT_PATH, '..'))
+else:
+    ROOT_PATH = path.abspath(path.join(CURRENT_PATH, '..', '..'))
 
 try:
     try:
         sys.path.remove(CURRENT_PATH)
     except:
-        print('ERR #1')
         pass
 
     sys.path.index(ROOT_PATH)
 
 except ValueError:
-    print('ERR #2')
     sys.path.insert(0, ROOT_PATH)
-
-print('sys.path', sys.path, CURRENT_PATH, ROOT_PATH)
 
 from rootpath.tests import helper
 
