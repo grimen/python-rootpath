fae9dec46a02eb2cbb49cc1ec9f1ca5b374b55a9
Jonas Grimfelt <grimen@gmail.com>
2019-03-13 05:06:48 -0400

    refactor: cleanup `setup.py` using `setupextras`

--- setup.py
+++ setup.py
@@ -1,69 +1,14 @@
 
-
 # =========================================
 #       IMPORTS
 # --------------------------------------
 
 import os
-import glob
 import setuptools
+import setupextras
 
 # DISABLED/BUG: this line fails when `pip install palmtree` but works `pip install .`
 # from palmtree import __version__
-
-
-# =========================================
-#       FUNCTIONS
-# --------------------------------------
-
-def find_data_files(data_file_patterns = [], root_path = None):
-    root_path = root_path or os.path.abspath(os.path.dirname(__file__))
-    data_file_dirs = []
-
-    for root, dirs, files in os.walk(root_path):
-        data_file_dirs.append(root)
-
-    data_files = []
-
-    for data_file_dir in data_file_dirs:
-        files = []
-
-        for data_file_pattern in data_file_patterns:
-            files += glob.glob(os.path.join(data_file_dir, data_file_pattern))
-
-        if not files:
-            continue
-
-        target = os.path.join(root_path, data_file_dir)
-
-        data_files.append((target, files))
-
-    return data_files
-
-def get_readme():
-    root_path = os.path.abspath(os.path.dirname(__file__))
-    readme_file_path = os.path.join(root_path, 'README.md')
-
-    with open(readme_file_path) as file:
-        readme = file.read()
-
-    return readme
-
-def get_requirements():
-    root_path = os.path.abspath(os.path.dirname(__file__))
-    requirements_file_path = os.path.join(root_path, 'requirements.txt')
-
-    with open(requirements_file_path) as file:
-        requirements = [requirement.strip() for requirement in file.readlines()]
-        requirements = filter(lambda requirement: len(requirement), requirements)
-        requirements = list(requirements)
-
-    return requirements
-
-# import setuptools
-
-# DISABLED/BUG: this line fails when `pip install rootpath` but works `pip install .`
-# from rootpath import __version__
 
 # =========================================
 #       MAIN
@@ -88,10 +33,10 @@ keywords = [
     'package-root-path',
 ]
 
-readme = get_readme()
-requirements = get_requirements()
-packages = setuptools.find_packages()
-data_files = find_data_files(['*.*'], os.path.join(name, 'tests', '__fixtures__'))
+packages = setupextras.get_packages()
+data_files = setupextras.get_data_files(['*.*'], os.path.join(name, 'tests', '__fixtures__'))
+requirements = setupextras.get_requirements()
+readme = setupextras.get_readme()
 
 config = {
     'name': name,
